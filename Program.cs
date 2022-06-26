8fcb270ee10ea2e3ffacd9e46b09d080400377aa
JaraMohnson <jaramohnson@gmail.com>
2022-06-26 17:50:21 -0400

    added check for blank input

--- Program.cs
+++ Program.cs
@@ -3,8 +3,14 @@
         Console.WriteLine("Please enter a word");
 
         string input = Console.ReadLine();
+        if (input == "")
+    {
+        Console.WriteLine("Please enter text to translate");
+        continue;
 
-        char first = input[0];
+    }
+
+    char first = input[0];
 
         int vowelPosition = -1; // keeping track of where my first vowel is; starting at -1 so it gets to 0 with first loop through
         string newStart = ""; // first letters of the new word 
@@ -13,30 +19,31 @@
         char[] letterBank = new char[] { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
 
 
-        if (letterBank.Contains(first))
-        {
-            Console.WriteLine(input + "way");
-        }
+    if (letterBank.Contains(first))
+    {
+        Console.WriteLine(input + "way");
+    }
 
-        else // starts with consonant     
+
+    else // starts with consonant     
+    {
+        foreach (char letter in input)
         {
-            foreach (char letter in input)
+            vowelPosition++;
+            if (letterBank.Contains(letter))
             {
-                vowelPosition++;
-                if (letterBank.Contains(letter))
-                {
-                    moveThis = input.Substring(0, vowelPosition); // saves the letters to be moved as its own string 
+                moveThis = input.Substring(0, vowelPosition); // saves the letters to be moved as its own string 
 
-                    int remainingLetters = (input.Length - moveThis.Length); // to make sure all remaining letters are accounted for after I substring out "move this"
+                int remainingLetters = (input.Length - moveThis.Length); // to make sure all remaining letters are accounted for after I substring out "move this"
 
-                    newStart = input.Substring(vowelPosition, remainingLetters); // this starts at vowel position, and takes all remaining letters as rest of substring
+                newStart = input.Substring(vowelPosition, remainingLetters); // this starts at vowel position, and takes all remaining letters as rest of substring
 
-                    Console.WriteLine(newStart + moveThis + "ay");
-                    break; // exit the loop once we find the first vowel 
+                Console.WriteLine(newStart + moveThis + "ay");
+                break; // exit the loop once we find the first vowel 
 
-                }
             }
         }
+    }
 
         Console.WriteLine("Do you want to translate another word? y/n ");
         string restart = Console.ReadLine().ToLower();
